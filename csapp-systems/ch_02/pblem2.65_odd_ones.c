45137fb98f5d072ab45f9b86ed6f5829612ce3e6
simbarashe-danda <dandasimbarashe21@gmail.com>
2026-04-29 00:01:06 +0200

    determining parity of bit 1s in an integer.

--- csapp-systems/ch_02/pblem2.65_odd_ones.c
+++ csapp-systems/ch_02/pblem2.65_odd_ones.c
@@ -15,21 +15,21 @@ int main(void)
     printf("%d\n", odd_ones(-1)); // conversion to U_max. must be 0
     printf("%d\n", odd_ones(0x7FFFFFFFu)); // T_max, must be 1
 }
-/* This comes from realization that given bit sequence Y = [b3, b2, b1, b0 ],
-b3^b2^b1^b0 will determine parity of the number of 1s in Y.
-But doing this linearly is inefficient , O(N) time where N = word size W
+/* This comes from realization that given bit sequence Y = [b_w-1, b_w-2,..., b1, b0 ],
+b_w-1 ^ b_w-2 ^ ... ^ b1 ^ b0 will determine parity of the number of bit 1s in Y.
+But doing this linearly is inefficient , O(W) time where W is word size
 
 Below code is inspired by divide and conquer.
 It comes from realizing that given |Y| = 2 where Y is bit vector, then
-Y ^ (Y >> 2) is like folding the vector in half then apply xor,
-in other words we appky XOR of half the bits which are right most bits against half which left most.
+Y ^ (Y >> 2) is like folding the vector in half then apply XOR,
+in other words we apply XOR of half the bits which are right most bits against half which left most.
 
-Final realization was that shifting Y' >> k and appling xor until k = 1 result in least significant bit,
-Given Z = Y' ^ (Y >> 1)
+Final realization was that shifting Y' >> k and appling XOR until last step k = 1:
+expressed as Z = Y' ^ (Y >> 1), then
 z0 represents our desired expression, that is to say
 z0 = (b_w-1 ^ b_w-2 ^ ....^ b_1 ^ b_0).
 
-This is O(log_2_N) time where N = word size W
+This is O(log_2_W) time where W is word size
 */
 
 int odd_ones(unsigned x) {
